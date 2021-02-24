830006ae21c6edb040506b7de92859834eee5006
Naveenkumar <naveenkm36@gmail.com>
2021-02-24 16:44:19 +0530

    Stud

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import admin_detail
+from .models import *
 
 # Register your models here.
 admin.site.register(admin_detail)
+admin.site.register(student_detail)
--- Admin/serializers.py
+++ Admin/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import admin_detail
+from .models import *
 
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = admin_detail
         fields = ['id', 'username', 'password', 'role','mobile']
 
+#serializer for student module
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = student_detail
+        fields = ['id','Rollno','Name','Type','Batch']
