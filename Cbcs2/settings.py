81cb509ef9423082bce239c7fbbfcd0d566b1772
Naveen kumar <50633984+Naveenkumar122@users.noreply.github.com>
2021-03-18 18:42:44 +0530

    Update settings.py

--- Cbcs2/settings.py
+++ Cbcs2/settings.py
@@ -131,5 +131,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT    = 587
 EMAIL_HOST_USER     = '18mca14@americancollege.edu.in'
-EMAIL_HOST_PASSWORD = 'Narmatha06' 
+EMAIL_HOST_PASSWORD = '' 
 
